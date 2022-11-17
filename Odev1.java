7fb3a8d32f32266bd70441d48392171079856868
UmutErenKaplan <109959111+UmutErenKaplan@users.noreply.github.com>
2022-11-17 12:35:44 +0300

    Add files via upload

---
+++ Odev1.java
@@ -0,0 +1,63 @@
+package com.example.javadersleri.degiskenler;
+//Umut Eren Kaplan
+public class Odev1 {
+    public static void main(String[] args) {
+        String sehir = "İZMİR";
+        String ulke = "TÜRKİYE";
+        String telefon = "0212 978 55 12";
+        int postaKodu = 20150;
+        String email = "qwerty@gmail.com";
+        String meslek = "Makine Mühendisi";
+        String stokMiktari = "5.5 kg şeker kaldı.";
+        String musteriAdi = "Selim";
+        double bakiye = 1780.12;
+        String dogumGunu = "Müşterimiz 16.11.2000 tarihinde dogmustur.";
+        double maas = 6000.67;
+        String medeniDurum = "Bekar";
+        String urunYorum = "Ürün beklediğim gibi değildi.Beğenmedim";
+        String odemeTarihi = "16.12.2021 tarihinde ürün ödemesi gerçekleşmiştir";
+        String odeme = "Ürüne 1211.11 Tl ödenmiştir";
+        int siparisAdeti = 100;
+        String arabaModeli="Audi A5 2022 Model";
+        String kitapAdi = "Macbeth";
+        String yayinlanmaTarihi = "22.08.2013 tarihinde yayınlandı";
+        String indirimMiktari = "%45 indirim";
+        int odaSayisi = 10;
+        String enlem = "Kuzey enleminde bulunan Ankara’nın yeri; Ekvator’dan 39 derece 56 dakika 25 saniye kuzeyde bulunur.";
+        String boylam  ="Ankara 32° 50' doğu boylamı koordinatında bulunur.";
+        String urunAdi = "Şarj Aleti";
+        double yemekFiyati = 65.99;
+        String marka = "BMW";
+        String muzikAdi = "Kış Güneşi-Tarkan";
+        String videoSuresi = "36:18 dk";
+        double urunPuani = 8.89;
+        String resimAdi = "The Starry Night";
+        String dosyaFormati = ".JPG";
+        String renk = "Mor";
+        String renkKodu = "483D8B";
+        String telefonModeli = "Samsung Galaxy A22 2022 Model";
+        String ekranBoyutu = "6.2 inch";
+        int agirlik = 140;
+        String ulusalGun = "30 Ağustos Zafer Bayramı";
+        String tatilGunu = "29 Haziran 2023 Perşembe Kurban Bayramı";
+        String rezervasyonTarihi = "22 Haziran 2023 Perşembe Günü";
+        String sokakAdi = "Sakarya Sokak";
+        int otobusHatti = 590;
+        double kalanDakika = 1.23;
+        int takipKodu = 155789;
+        String kuponSuresi = "21.11.2022 tarihine kadar geçerlidir";
+        int kuponKodu = 12351354;
+        String faturaAdresi = " Alev Apt no 8 666.sokak Şahintepe Mah. Mamak/Ankara";
+
+        /*System.out.println(sehir + ulke + telefon + postaKodu + email + meslek);
+        System.out.println(stokMiktari + musteriAdi + bakiye + dogumGunu + maas + medeniDurum);
+        System.out.println(urunYorum + odemeTarihi+ odeme + siparisAdeti + arabaModeli+ kitapAdi);
+        System.out.println(yayinlanmaTarihi + indirimMiktari+ odaSayisi  + enlem + boylam);
+        System.out.println(urunAdi + yemekFiyati + marka + muzikAdi + videoSuresi + urunPuani);
+        System.out.println(resimAdi+ dosyaFormati + renk + renkKodu + telefonModeli + ekranBoyutu);
+        System.out.println(agirlik + ulusalGun + tatilGunu + rezervasyonTarihi + sokakAdi + otobusHatti);
+        System.out.println(kalanDakika + takipKodu + kuponSuresi + kuponKodu + faturaAdresi );*/
+
+
+    }
+}
